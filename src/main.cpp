5b1853b8c0f73841030c48aa802e295a6a33c2f6
Dave Nicholson <fatnic388@gmail.com>
2018-09-17 21:10:59 +0100

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <GLFW/glfw3.h>
+
+ int main()
+{
+     GLFWwindow* window;
+
+     if (!glfwInit())
+         return -1;
+
+     window = glfwCreateWindow(640, 360, "GLFWWindow", NULL, NULL);
+     if (!window) {
+         glfwTerminate();
+         return -1;
+     }
+
+     glfwMakeContextCurrent(window);
+
+     while(!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+     }
+
+     return 0;
+}
